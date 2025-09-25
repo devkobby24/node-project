2d0f7d4c283c3b73b7d0cb93fe2c970a0d0c7a47
devkobby24 <nanakobby2002@gmail.com>
2025-09-25 13:43:54 +0000

    refactor: reorganize product API routes and remove test endpoint

--- index.js
+++ index.js
@@ -9,10 +9,30 @@ const Product = require('./models/product.model.js');
 
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.send("Hello World. I'm testing to see what happens when I change this text. Did it?");
+//get all products
+app.get('/api/get-products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server Error');
+    }
 });
 
+
+app.get('/api/get-product/:id', async (req, res) => {
+    try {
+        const products = await Product.findById(req.params.id);
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//create a product
 app.post('/api/add-products', async (req, res) => {
     try {
         const newProduct = await Product.create(req.body);
@@ -25,6 +45,7 @@ app.post('/api/add-products', async (req, res) => {
 
 app.listen(3000);
 
+//connect to db
 mongoose.connect(dbconnect)
     .then(() => console.log('Connected to DB succcessfully'))
     .catch(err => console.log(err));
