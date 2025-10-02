c6460df731b38c9a640bac64520296dae1b77493
devkobby24 <nanakobby2002@gmail.com>
2025-10-02 13:04:07 +0000

    feat: add product routes for CRUD operations

---
+++ routes/product.route.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const { getProducts, getProductById, updateProductById, updateManyProducts, deleteAllMatchingProducts, deleteProductByFilter, deleteProductById, createProduct } = require('../controllers/product.controller.js');
+
+router.get('/get-product', getProducts);
+
+router.get('/get-product/:id', getProductById);
+
+router.post('/add-product', createProduct);
+
+router.put('/update-product/:id', updateProductById);
+
+router.put('/update-many', updateManyProducts);
+
+router.delete('/delete-all', deleteAllMatchingProducts);
+
+router.delete('/delete-filter', deleteProductByFilter);
+
+router.delete('/delete/:id', deleteProductById);
+
+
+
+
+module.exports = router;
