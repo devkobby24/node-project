235f3ffeafba5578482466f0806a2e47c4835348
devkobby24 <nanakobby2002@gmail.com>
2025-10-02 13:03:53 +0000

    feat: update product routes for consistency and improve endpoint naming

--- index.js
+++ index.js
@@ -5,110 +5,119 @@ dotenv.config();
 const app = express();
 const dbconnect = process.env.DATABASE_URL;
 const Product = require('./models/product.model.js');
+const productRoutes = require('./routes/product.route.js');
 
 
+//middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api", productRoutes);
+
+
+
 
 //get all products
-app.get('/api/get-products', async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server Error');
-    }
-});
+// app.get('/api/get-product', async (req, res) => {
+//     try {
+//         const products = await Product.find();
+//         res.status(200).json(products);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send('Server Error');
+//     }
+// });
 
 //getting a product by id
-app.get('/api/get-product/:id', async (req, res) => {
-    try {
-        const products = await Product.findById(req.params.id);
-        res.status(200).json(products);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server Error');
-    }
-});
+// app.get('/api/get-product/:id', async (req, res) => {
+//     try {
+//         const products = await Product.findById(req.params.id);
+//         res.status(200).json(products);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send('Server Error');
+//     }
+// });
 
 //update a product by id
-app.put('/api/update-product/:id', async (req, res) => {
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).json(updatedProduct);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server Error');
-    }
-});
+// app.put('/api/update-product/:id', async (req, res) => {
+//     try {
+//         const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//         res.status(200).json(updatedProduct);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send('Server Error');
+//     }
+// });
 
 //update multiple documents
-app.put('/api/update-many', async (req, res) => {
-    try {
-        const { filter, update } = req.body;
+// app.put('/api/update-many', async (req, res) => {
+//     try {
+//         const { filter, update } = req.body;
 
-        const updatedProducts = await Product.updateMany(filter, update);
+//         const updatedProducts = await Product.updateMany(filter, update);
 
-        res.status(200).json(updatedProducts);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred");
-    }
-});
+//         res.status(200).json(updatedProducts);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("An error occurred");
+//     }
+// });
 
 
 
 //create a product
-app.post('/api/add-products', async (req, res) => {
-    try {
-        const newProduct = await Product.create(req.body);
-        res.status(201).json(newProduct);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server Error');
-    }
-});
+// app.post('/api/add-product', async (req, res) => {
+//     try {
+//         const newProduct = await Product.create(req.body);
+//         res.status(201).json(newProduct);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send('Server Error');
+//     }
+// });
 
 //delete a product by filter
-app.delete('/api/delete-first-match', async (req, res) => {
-    try {
-        const deletedProduct = await Product.deleteOne(req.body);
-        res.status(201).json(deletedProduct)
-    }
-    catch (error) {
-        res.status(500).send('An Error Occured')
-    }
-});
+// app.delete('/api/delete-first-match', async (req, res) => {
+//     try {
+//         const deletedProduct = await Product.deleteOne(req.body);
+//         res.status(201).json(deletedProduct)
+//     }
+//     catch (error) {
+//         res.status(500).send('An Error Occured')
+//     }
+// });
 
 //delete all matching products by filter
-app.delete('/api/delete-all', async (req, res) => {
-    try {
-        const deleteAllProducts = await Product.deleteMany(req.body);
-        res.status(201).json(deleteAllProducts)
+// app.delete('/api/delete-all', async (req, res) => {
+//     try {
+//         const deleteAllProducts = await Product.deleteMany(req.body);
+//         res.status(201).json(deleteAllProducts)
 
-        if (!deleteAllProducts) {
-            res.send("Products doesn't exist")
-        }
-    }
-    catch (error) {
-        res.status(500).send('An Error Occured')
-    }
-});
+//         if (!deleteAllProducts) {
+//             res.send("Products doesn't exist")
+//         }
+//     }
+//     catch (error) {
+//         res.status(500).send('An Error Occured')
+//     }
+// });
 
 //delete a product by id
-app.delete('/api/delete/:id', async (req, res) => {
-    try {
-        const deletedProductById = await Product.findByIdAndDelete(req.params.id);
-        if (!deletedProductById) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-        res.status(200).json(deletedProductById)
-    }
-    catch (error) {
-        console.log(error)
-        res.status(500).send('An Error Occured')
-    }
-});
+// app.delete('/api/delete/:id', async (req, res) => {
+//     try {
+//         const deletedProductById = await Product.findByIdAndDelete(req.params.id);
+//         if (!deletedProductById) {
+//             return res.status(404).json({ message: 'Product not found' });
+//         }
+//         res.status(200).json(deletedProductById)
+//     }
+//     catch (error) {
+//         console.log(error)
+//         res.status(500).send('An Error Occured')
+//     }
+// });
 
 app.listen(3000);
 
